b12ab12dd756a580a496fe8a66506e80e8c2739b
cryptointi <cryptointi@gmail.com>
2018-05-20 01:05:21 -0400

    se borra la carpeta /routes

--- routes/Copia de index.js
+++
@@ -1,9 +0,0 @@
-var ctrl = require('../app-server/controllers/main');
-
-console.log("pre index.js");
-
-module.exports = function (app) {
-	console.log("index.js Antes");
-  app.get('/', ctrl);
-  console.log("index.js Despues");
-};
